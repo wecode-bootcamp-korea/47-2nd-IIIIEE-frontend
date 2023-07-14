5675a45a58b7f1e858d19a2581913292de36a6e8
양회진 <yanghoejin@yanghoejin-ui-MacBookAir.local>
2023-07-14 20:00:29 +0900

    초기세팅 2차

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,4 +1,12 @@
-const GlobalStyle = () => {
-  return;
-};
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+  * {
+    box-sizing: border-box;
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+`;
+
 export default GlobalStyle;
